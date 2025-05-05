23da752bce9c2608ac84ab0f6a23d702193ba624
kumar <rahuldolai004@gmail.com>
2025-05-05 18:05:49 +0530

    Adding todo component some changes

--- src/firebase-config/index.js
+++ src/firebase-config/index.js
@@ -1,15 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// Firebase configuration from environment variables
 const firebaseConfig = {
-    apiKey: "AIzaSyDletUi2q8wRQ1ktjfDRQnN2oBokXLkSvI",
-    authDomain: "react-projects-firebase-todo.firebaseapp.com",
-    projectId: "react-projects-firebase-todo",
-    storageBucket: "react-projects-firebase-todo.firebasestorage.app",
-    messagingSenderId: "947063166989",
-    appId: "1:947063166989:web:0dd82af10754b429d15d44",
-    measurementId: "G-47NWB355FX"
-  };
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+};
+
 
   const firebaseApp = initializeApp(firebaseConfig);
   export const db = getFirestore(firebaseApp);
